219bdcc078049f320b1392c0ac177ea43c4bc8d0
Kiera Nguyen <ngannguyen9017@gmail.com>
2018-01-27 15:38:18 -0500

    oldContent

--- extension-skeleton/content.js
+++ extension-skeleton/content.js
@@ -7,20 +7,7 @@ for (var i = 0; i < elements.length; i++) {
         var node = element.childNodes[j];
 
         if (node.nodeType === 3) {
-            var request = new XMLHttpRequest();
             var text = node.nodeValue;
-
-            request.onreadystatechange = function(){
-              if(this.readyState == 4 && this.status == 200){
-                text.innerHTML = this.responseText;
-              }else{
-                text.innerHTML = " ";
-              }
-            }
-
-            request.open('POST', '/', true);
-            request.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
-            request.send(text);
 
 
             var replacedText = text.replace(/General/gi, '-------------');
