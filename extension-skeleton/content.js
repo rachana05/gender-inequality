72bec620a786b2c0656e4ff63ccc8a5990bd6c71
Yuchen Jin <31979100+yjin8@users.noreply.github.com>
2018-01-27 20:05:28 -0500

    added funny data

--- extension-skeleton/content.js
+++ extension-skeleton/content.js
@@ -1,5 +1,5 @@
 var elements = document.getElementsByTagName('*');
-var req = new XMLHttpRequest();
+//var req = new XMLHttpRequest();
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
 
@@ -11,30 +11,38 @@ for (var i = 0; i < elements.length; i++) {
             text = text.toLowerCase();
             var origText = text;
 
-            var badWords = ["trump","weinstein","fag","slut","bitch"]
-            var a = badWords.indexOf(origText);
+            //var badWords = ["trump","weinstein","fag","slut","bitch"]
+            //var a = badWords.indexOf(origText);
 
             
-            if (a !== -1) {
-                text = text.replace(/badWords[a]/gi, 'moron');
-            }
-            /*
-            text = text.replace(/donald trump/gi, 'moron');
-            text = text.replace(/weinstein/gi, 'ew');
-            text = text.replace(/fag/gi, ':( :(');
-            text = text.replace(/slut/gi, 'I am a queen');
-
-
-            /*
-            text = text.replace(/\bTrump\b/g, '-------------');
-            text = text.replace(/\bTrump\b/g, '-------------');
-            text = text.replace(/\bTrump\b/g, '-------------');
-            */
-
-
-            //if (text !== origText) {
-                element.replaceChild(document.createTextNode(text), node);
+            //if (a !== -1) {
+            //    text = text.replace(/badWords[a]/gi, 'moron');
             //}
+            var quotes = ["To succeed in life, you need three things: a wishbone, a backbone and a funny \
+            bone", "Nothing is impossible. The word itself says I'm possible!", "Banana", "Orange", "Apple",
+            "Live Laugh Love", ":( :( :(", "Juicy Couture", "~*~*~Ghéy~*~*~", "✧・ﾟ:* ", " (ﾉ◕ヮ◕)ﾉ*:・ﾟ✧",
+            "Bed Bath & Beyond", "Cheeto", "Nachos","Taco", "Burrito","dumpling"
+
+
+            ]
+            
+            var randInt = Math.floor(Math.random() * quotes.length)
+
+            text = text.replace(/trump/gi, quotes[randInt]);
+            text = text.replace(/weinstein/gi, quotes[randInt]);
+            text = text.replace(/fag/gi, quotes[randInt]);
+            text = text.replace(/slut/gi, quotes[randInt]);
+            text = text.replace(/dyke/gi, quotes[randInt]);
+            text = text.replace(/bitch/gi, quotes[randInt]);
+            text = text.replace(/pussy/gi, quotes[randInt]);
+            text = text.replace(/p\*ssy/gi, quotes[randInt]);
+            text = text.replace(/penis/gi, quotes[randInt]);
+
+
+
+            if (text !== origText) {
+                element.replaceChild(document.createTextNode(text), node);
+            }
         }
     }
 }
